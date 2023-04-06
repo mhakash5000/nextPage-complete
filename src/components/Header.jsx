42428a2ba2ccc5613d25942be4370e444473d5a9
MD.Makbul Hosen <56879293+mhakash5000@users.noreply.github.com>
2023-04-06 12:22:23 +0600

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,118 +1,109 @@
-import React, { useState } from 'react'
-import { Link, NavLink } from 'react-router-dom'
-import {
-  BoltIcon,
-  Bars3BottomRightIcon,
-  XMarkIcon,
-} from '@heroicons/react/24/solid'
+import React, { useState } from "react";
+import Logo from '../assets/logo.png'
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
-  return (
-    <div className='bg-gray-100 px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'>
-      <div className='relative flex items-center justify-between'>
-        {/* Logo Section */}
-        <Link to='/' className='inline-flex items-center'>
-          <BoltIcon className='h-6 w-6 text-blue-500' />
-          <span className='ml-2 text-xl font-bold tracking-wide text-gray-800'>
-            nextPage
-          </span>
-        </Link>
+    const location = useLocation();
+    const [isOpen, setIsOpen] = useState(false);
 
-        {/* Nav Items Section */}
-        <ul className='items-center hidden space-x-8 lg:flex'>
-          <li>
-            <NavLink
-              to='/'
-              className={({ isActive }) => (isActive ? 'active' : 'default')}
+    const toggleMenu = () => {
+      setIsOpen(!isOpen);
+    };
+  return (
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex-shrink-0">
+            <Link to="/">
+              <img className="h-8 w-8" src="/logo.svg" alt="Logo" />
+            </Link>
+          </div>
+          <div className="hidden md:block">
+            <div className="ml-10 flex items-baseline space-x-4">
+              <Link
+                to="/"
+                className={`text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${
+                  location.pathname === "/"
+                    ? "bg-yellow-500 text-white"
+                    : "text-gray-300"
+                }`}
+              >
+                Home
+              </Link>
+              <Link
+                to="/about"
+                className={`text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${
+                  location.pathname === "/about"
+                    ? "bg-yellow-500 text-white"
+                    : "text-gray-300"
+                }`}
+              >
+                About
+              </Link>
+            </div>
+          </div>
+          <div className="-mr-2 flex md:hidden">
+            <button
+              onClick={toggleMenu}
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out"
+            >
+              <svg
+                className={`${isOpen ? "hidden" : "block"} h-6 w-6`}
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+              <svg
+                className={`${isOpen ? "block" : "hidden"} h-6 w-6`}
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div className={`${isOpen ? "block" : "hidden"} md:hidden`}>
+          <div className="px-2 pt-2 pb-3 sm:px-3">
+            <Link
+              to="/"
+              className={`text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium ${
+                location.pathname === "/"
+                  ? "bg-yellow-500 text-white"
+                  : "text-gray-300"
+              }`}
             >
               Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to='/books'
-              className={({ isActive }) => (isActive ? 'active' : 'default')}
+            </Link>
+            <Link
+              to="/about"
+              className={`text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium ${
+                location.pathname === "/about"
+                  ? "bg-yellow-500 text-white"
+                  : "text-gray-300"
+              }`}
             >
-              Books
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to='/about'
-              className={({ isActive }) => (isActive ? 'active' : 'default')}
-            >
-              About us
-            </NavLink>
-          </li>
-        </ul>
-        {/* Mobile Navbar Section */}
-        <div className='lg:hidden'>
-          {/* Dropdown Open Button */}
-          <button
-            aria-label='Open Menu'
-            title='Open Menu'
-            onClick={() => setIsMenuOpen(true)}
-          >
-            <Bars3BottomRightIcon className='w-5 text-gray-600' />
-          </button>
-          {isMenuOpen && (
-            <div className='absolute top-0 left-0 w-full z-10'>
-              <div className='p-5 bg-white border rounded shadow-sm'>
-                {/* Logo & Button section */}
-                <div className='flex items-center justify-between mb-4'>
-                  <div>
-                    <Link to='/' className='inline-flex items-center'>
-                      <BoltIcon className='h-6 w-6 text-blue-500' />
-                      <span className='ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase'>
-                        nextPage
-                      </span>
-                    </Link>
-                  </div>
-                  {/* Dropdown menu close button */}
-                  <div>
-                    <button
-                      aria-label='Close Menu'
-                      title='Close Menu'
-                      onClick={() => setIsMenuOpen(false)}
-                    >
-                      <XMarkIcon className='w-5 text-gray-600' />
-                    </button>
-                  </div>
-                </div>
-                {/* Mobile Nav Items Section */}
-                <nav>
-                  <ul className='space-y-4'>
-                    <li>
-                      <Link to='/' className='default'>
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        to='/books'
-                        className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
-                      >
-                        Books
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        to='/about'
-                        className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
-                      >
-                        About Us
-                      </Link>
-                    </li>
-                  </ul>
-                </nav>
-              </div>
-            </div>
-          )}
+              About
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-  )
-}
+    </nav>
+  );
+};
 
-export default Header
+export default Header;
